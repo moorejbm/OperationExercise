b239220143eb469c0c1e1cb9a5e1ff617e21eb3a
Jason Moore <moorejbm@gmail.com>
2021-01-22 18:01:33 -0600

    OpEx

---
+++ OperationExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OperationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //explicit typing -
+            string variableName = "Hello";
+
+            //inferred typing -
+            var varName = "Hello";
+
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;                     
+                  
+           Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with radius of {r} is {areaOfCircle}");
+        }
+    }
+}
